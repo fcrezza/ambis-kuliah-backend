bf1e12bfbd666029e78754a726fb3221fc7c04f7
fcrezza <fachrezanang@gmail.com>
2021-01-14 18:47:37 +0700

    make logout route

--- src/Application/Actions/Auth/Logout.php
+++ src/Application/Actions/Auth/Logout.php
@@ -6,23 +6,20 @@ namespace App\Application\Actions\Auth;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LoggerInterface;
 
 use App\Application\Actions\Action;
 use App\Domain\User\UserRepository;
 
 class Logout extends Action {
-   private $container;
-
-    protected $userRepository;
-
-    public function __construct(LoggerInterface $logger, UserRepository $userRepository) {
+    public function __construct(LoggerInterface $logger) {
         parent::__construct($logger);
-        $this->userRepository = $userRepository;
     }
 
     protected function action(): ResponseInterface {
-      $data = $this->userRepository->findAll();
-      $this->logger->info("hit login route!");
-      return $this->respondWithData($topics);
+      setcookie("accessToken", "", time() - 3600, '/', 'localhost', false, true);
+      setcookie("refreshToken", "", time() - 3600, '/', 'localhost', false, true);
+      $this->logger->info("hit logout route");
+      return $this->respondWithData(["message" => "berhasil logout"]);
     }
 }
